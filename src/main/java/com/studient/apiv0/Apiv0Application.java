8406180b8e66f28212bd53e5dbea5b4de74ddc84
Ibrahima07 <ibrahimassissoko07@gmail.com>
2023-07-13 18:08:39 +0000

    Backend project API initialization

---
+++ src/main/java/com/studient/apiv0/Apiv0Application.java
@@ -0,0 +1,13 @@
+package com.studient.apiv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiv0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apiv0Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/studient/apiv0/Apiv0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studient.apiv0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apiv0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
